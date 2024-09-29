801e1e10d22f7e38b4efeae7d65da09a54ff9cc4
DevFaith <waitherafaith@outlook.com>
2024-09-29 23:54:57 +0300

    * chore(Footer.jsx): add missing semicolons and import necessary icons * feat(Footer.jsx): update footer component to include social media icons and copyright information

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { FaXTwitter, FaSquareInstagram, FaSquareFacebook, FaLinkedin } from 'react-icons/fa6';
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>
-      
+    <div className='footer bg-slate-700 flex justify-center items-center gap-8 py-4'>
+      <div className='flex gap-4 text-white text-2xl'>
+        <a href="https://x.com/faith_wait15653" target="_blank" rel="noopener noreferrer" className='hover:text-blue-500 transition-colors duration-300'>
+          <FaXTwitter />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className='hover:text-pink-500 transition-colors duration-300'>
+          <FaSquareInstagram />
+        </a>
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className='hover:text-blue-700 transition-colors duration-300'>
+          <FaSquareFacebook />
+        </a>
+        <a href="https://www.linkedin.com/in/faith-waithera-0a4b3329b/" target="_blank" rel="noopener noreferrer" className='hover:text-blue-600 transition-colors duration-300'>
+          <FaLinkedin />
+        </a>
+      </div>
+      <p className='text-white'>&copy; 2024 Sneaking For Life</p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
