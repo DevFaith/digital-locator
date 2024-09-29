24c80806b49825dcb244515af83e7da8f17ee537
DevFaith <waitherafaith@outlook.com>
2024-09-29 23:14:01 +0300

    * feat(App.jsx): add routing functionality and components for Header, Footer, Home, About, and Login pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-export default function App() {
+import React from 'react'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Login from './Pages/Login'
+
+function App() {
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+    
   )
 }
+
+export default App
